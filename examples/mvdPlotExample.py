starting to fix rotation bugs in hinv.  performance improve to hinv fn

examples/mvdPlotExample.py
@@ -1,9 +1,13 @@
 #!/usr/env/python2
 from __future__ import absolute_import, print_function, division
+from scipy.optimize import bisect, newton
+from scipy.stats import gaussian_kde
 import context
 from starvine.mvar import mvd
 import pandas as pd
 import numpy as np
+# copula imports
+import starvine.bvcopula as bvc
 
 
 def h5Load(store, grpName):
@@ -13,25 +17,26 @@ def h5Load(store, grpName):
     @param grpName HDF5 group name locating data in h5 store
     """
     data = store[grpName]
-    metadata = store.get_storer(grpName).attrs.metadata
-    return data, metadata
+    return data
+
 
 def main():
     # read data from external h5 file
     h5file = 'Cicada_cfd_180x_cht.h5.post.binned.h5'
     store = pd.HDFStore(h5file)
-    bounds = h5Load(store, "Water/UO2 [Interface 1]/Temperature_bounds")[0]
-    temperature = h5Load(store, "Water/UO2 [Interface 1]/Temperature")[0]
-    tke = h5Load(store, "Water/UO2 [Interface 1]/TurbulentKineticEnergy")[0]
-    crud_thick = h5Load(store, "Water/UO2 [Interface 1]/CrudThickness")[0]
-    b10 = h5Load(store, "Water/UO2 [Interface 1]/CrudBoronDensity")[0]
-    weight = h5Load(store, "Water/UO2 [Interface 1]/Temperature_weights")[0]
-    bhf = h5Load(store, "Water/UO2 [Interface 1]/BoundaryHeatFlux")[0]
+    bounds = h5Load(store, "Water/UO2 [Interface 1]/Temperature_bounds")
+    temperature = h5Load(store, "Water/UO2 [Interface 1]/Temperature")
+    tke = h5Load(store, "Water/UO2 [Interface 1]/TurbulentKineticEnergy")
+    crud_thick = h5Load(store, "Water/UO2 [Interface 1]/CrudThickness")
+    b10 = h5Load(store, "Water/UO2 [Interface 1]/CrudBoronDensity")
+    weight = h5Load(store, "Water/UO2 [Interface 1]/Temperature_weights")
+    bhf = h5Load(store, "Water/UO2 [Interface 1]/BoundaryHeatFlux")
     store.close()
 
+    """
     # create multi-variate dataset for span 1
     # for zone in range(68, 80):
-    for zone in range(69, 80):
+    for zone in range(71, 78):
         lower_b = bounds.values[:, zone][0]
         print("Generating plot for zone: " + str(zone))
         temps = temperature.values[:, zone][~np.isnan(temperature.values[:, zone])]
@@ -47,10 +52,11 @@ def main():
         span_1_mvd = mvd.Mvd()
         span_1_mvd.setData(span_1_dataDict, weights)
         span_1_mvd.plot(savefig="mvd_" + str(round(lower_b, 3)) + ".png", kde=False)
+    """
 
     # full span plot
     tsat = -618.5
-    zones = range(71, 78)
+    zones = range(71, 72)
     temps = temperature.values[:, zones][~np.isnan(temperature.values[:, zones])]
     tkes = tke.values[:, zones][~np.isnan(tke.values[:, zones])]
     cruds = crud_thick.values[:, zones][~np.isnan(crud_thick.values[:, zones])]
@@ -62,26 +68,46 @@ def main():
                        "Residual BHF [W/m^2]": bhfs,
                        }
     span_1_mvd = mvd.Mvd()
-    span_1_mvd.setData(span_1_dataDict, weights)
-    span_1_mvd.plot(savefig="mvd_span.png", kde=False)
+    #span_1_mvd.setData(span_1_dataDict, weights)
+    #span_1_mvd.plot(savefig="mvd_span.png", kde=False)
 
-    """
-    # create multi-variate dataset for span 2
-    span_2_dataDict = {"bounds": bounds[0].values[:,2],
-                       "temperature": temperature[0].values[:,2],
-                       "tke": tke[0].values[:,2],
-                       "crud": crud_thick[0].values[:,2],
-                       "b10": b10[0].values[:,2],
-                       }
+    # fit bivariate copula to span plot; T vs TKE:
+    copula = bvc.PairCopula(temps, tkes, family={'gumbel': 3})
+    copula.copulaTournament()
 
-    # create multi-variate dataset for span 3
-    span_3_dataDict = {"bounds": bounds[0].values[:,3],
-                       "temperature": temperature[0].values[:,3],
-                       "tke": tke[0].values[:,3],
-                       "crud": crud_thick[0].values[:,3],
-                       "b10": b10[0].values[:,3],
-                       }
-    """
+    # plot original
+    bvc.bvJointPlot(temps, tkes, savefig="t_tke_original.png")
+
+    # sample from copula
+    print("Copula Params: " + str(copula.copulaParams))
+    t_hat, tke_hat = copula.copulaModel.sample(500)
+    bvc.bvJointPlot(t_hat, tke_hat, savefig="t_tke_copula_sample.png")
+
+    rand_u = np.linspace(0.05, 0.95, 40)
+    rand_v = np.linspace(0.05, 0.95, 40)
+    u, v = np.meshgrid(rand_u, rand_v)
+    copula_pdf = copula.copulaModel.pdf(u.flatten(), v.flatten())
+    bvc.bvContourf(u.flatten(), v.flatten(), copula_pdf, savefig="t_tke_copula_pdf.png")
+
+    # Resample original data
+    def icdf_uv_bisect(ux, X, marginalCDFModel):
+        icdf = np.zeros(np.array(X).size)
+        for i, xx in enumerate(X):
+            kde_cdf_err = lambda m: xx - marginalCDFModel(-np.inf, m)
+            try:
+                icdf[i] = bisect(kde_cdf_err,
+                                 min(ux) - np.abs(0.5 * min(ux)),
+                                 max(ux) + np.abs(0.5 * max(ux)),
+                                 xtol=1e-2, maxiter=10)
+                icdf[i] = newton(kde_cdf_err, icdf[i], tol=1e-6, maxiter=20)
+            except:
+                icdf[i] = np.nan
+        return icdf
+    kde_cdf = gaussian_kde(temps).integrate_box
+    resampled_t = icdf_uv_bisect(temps, t_hat, kde_cdf)
+    kde_cdf = gaussian_kde(tkes).integrate_box
+    resampled_tke = icdf_uv_bisect(tkes, tke_hat, kde_cdf)
+    bvc.bvJointPlot(resampled_t, resampled_tke, savefig="t_tke_resampled.png")
 
 
 if __name__ == "__main__":

starvine/bvcopula/copula/clayton_copula.py
@@ -58,6 +58,7 @@ class ClaytonCopula(CopulaBase):
         h1 = -(1.0 + theta[0]) / theta[0]
         UU = np.array(u)
         VV = np.array(v)
+        # TODO: CHECK UU and VV ordering!
         uu = np.power(np.power(VV,theta[0])*(np.power(UU,-theta[0])-1.0)+1.0,h1);
         return uu
 
@@ -67,7 +68,8 @@ class ClaytonCopula(CopulaBase):
         h2 = -theta[0] / (1.0 + theta[0])
         UU = np.array(U)
         VV = np.array(V)
-        uu = np.power(np.power(VV,-theta[0])*(np.power(UU,h2)-1.0)+1.0,h1);
+        # TODO: CHECK UU and VV ordering!
+        uu = np.power(np.power(UU,-theta[0])*(np.power(VV,h2)-1.0)+1.0,h1);
         return uu
 
     @CopulaBase._rotGen

starvine/bvcopula/copula/copula_base.py
@@ -5,7 +5,7 @@
 from __future__ import print_function, absolute_import
 import numpy as np
 import scipy.integrate as spi
-from scipy.optimize import bisect
+from scipy.optimize import bisect, newton
 from scipy.optimize import minimize
 from scipy.misc import derivative
 
@@ -86,9 +86,11 @@ class CopulaBase(object):
         data pairs from copula with paramters: *theta
         """
         rotation = 0
-        u_hat = np.random.uniform(1e-9, 1 - 1e-9, n)
-        v = np.random.uniform(1e-9, 1 - 1e-9, n)
-        v_hat = self._hinv(u_hat, v, rotation, *mytheta)
+        u_iid_uniform = np.random.uniform(1e-9, 1 - 1e-9, n)
+        v_iid_uniform = np.random.uniform(1e-9, 1 - 1e-9, n)
+        # sample from copula
+        u_hat = u_iid_uniform
+        v_hat = self._hinv(u_iid_uniform, v_iid_uniform, rotation, *mytheta)
         return (u_hat, v_hat)
 
     def setRotation(self, rotation=0):
@@ -201,11 +203,14 @@ class CopulaBase(object):
     def _invhfun_bisect(self, U, V, rotation, *theta):
         """!
         @brief Compute inverse of H function using bisection.
-        TODO: Improve performance: finish with newton iterations
+        Update (11/08/2016) performance improvement: finish with newton iterations
         """
         # Freeze U, V, rotation, and model parameter, theta
         reducedHfn = lambda u: self._h(u, V, rotation, *theta) - U
-        return bisect(reducedHfn, 1e-10, 1.0 - 1e-10, maxiter=500)[0]
+        # return bisect(reducedHfn, 1e-10, 1.0 - 1e-10, maxiter=500)
+        v_bisect_est_ = bisect(reducedHfn, 1e-10, 1.0 - 1e-10, maxiter=10)
+        v_newton_est_ = newton(reducedHfn, v_bisect_est_, maxiter=30)
+        return v_newton_est_
 
     def _AIC(self, u, v, rotation=0, *theta):
         """!
@@ -329,13 +334,13 @@ class CopulaBase(object):
                 return f(self, u, v, rot, *nargs, **kwargs)
             elif self.rotation == 1:
                 # 90 deg rotation
-                return 1. - f(self, 1. - u, v, rot, *nargs)
+                return f(self, u, 1. - v, rot, *nargs)
             elif self.rotation == 2:
                 # 180 deg rotation
                 return 1. - f(self, 1. - u, 1. - v, rot, *nargs)
             elif self.rotation == 3:
                 # 270 deg rotation
-                return f(self, u, 1. - v, rot, *nargs)
+                return 1. - f(self, u, 1. - v, rot, *nargs)
         return wrapper
 
     @classmethod
@@ -355,13 +360,13 @@ class CopulaBase(object):
                 return f(self, u, v, rot, *nargs, **kwargs)
             elif self.rotation == 1:
                 # 90 deg rotation
-                return 1. - f(self, 1. - u, v, rot, *nargs)
+                return f(self, u, 1. - v, rot, *nargs)
             elif self.rotation == 2:
                 # 180 deg rotation
                 return 1. - f(self, 1. - u, 1. - v, rot, *nargs)
             elif self.rotation == 3:
                 # 270 deg rotation
-                return f(self, u, 1. - v, rot, *nargs)
+                return 1. - f(self, u, 1. - v, rot, *nargs)
         return wrapper
 
     @classmethod

starvine/bvcopula/copula/gauss_copula.py
@@ -81,10 +81,14 @@ class GaussCopula(CopulaBase):
         """!
         @brief H function (Conditional distribution) of Gauss copula.
         """
+        kT = self.kTau(*theta)
+        kTs = kT / abs(kT)
+        kTM = 1 if kTs < 0 else 0
+
         h1 = np.sqrt(1.0 - np.power(np.array(theta[0]), 2))
         dist = stats.norm(scale=1.0, loc=0.0)
 
-        UU = np.array(u)  # TODO: check input bounds
+        UU = np.array(kTM + kTs * u)  # TODO: check input bounds
         VV = np.array(v)
 
         # inverse CDF yields quantiles
@@ -96,14 +100,19 @@ class GaussCopula(CopulaBase):
         return uu
 
     @CopulaBase._rotHinv
-    def _hinv(self, u, v, rotation=0, *theta):
+    def _hinv(self, v, u, rotation=0, *theta):
         """!
         @brief Inverse H function (Inv Conditional distribution) of Gauss copula.
         """
+        # TODO: check v, u ordering!
+        kT = self.kTau(*theta)
+        kTs = kT / abs(kT)
+        kTM = 1 if kTs < 0 else 0
+
         h1 = np.sqrt(1.0 - np.power(np.array(theta[0]), 2))
         dist = stats.norm(scale=1.0, loc=0.0)
 
-        UU = np.array(u)  # TODO: check input bounds
+        UU = np.array(kTM + kTs * u)  # TODO: check input bounds
         VV = np.array(v)
 
         # inverse CDF yields quantiles

starvine/bvcopula/copula/gumbel_copula.py
@@ -68,15 +68,15 @@ class GumbelCopula(CopulaBase):
         return uu
 
     @CopulaBase._rotHinv
-    def _hinv(self, u, v, rotation=0, *theta):
+    def _hinv(self, v, u, rotation=0, *theta):
         """!
         TODO: Computing hinv by bisection is slow. speed up needed!
         """
         U = np.array(u)
         V = np.array(v)
         uu = np.zeros(U.size)
-        for i, (uu, vv) in enumerate(zip(U, V)):
-            uu[i] = self._invhfun_bisect(uu, vv, rotation, *theta)
+        for i, (ui, vi) in enumerate(zip(U, V)):
+            uu[i] = self._invhfun_bisect(ui, vi, rotation, *theta)
         return uu
 
     @CopulaBase._rotGen

starvine/bvcopula/pc_base.py
@@ -8,7 +8,7 @@ from scipy.stats import kendalltau, spearmanr, pearsonr
 from scipy.stats import gaussian_kde
 from scipy.stats.mstats import rankdata
 # COPULA IMPORTS
-from copula_factory import Copula
+from starvine.bvcopula.copula_factory import Copula
 
 
 class PairCopula(object):