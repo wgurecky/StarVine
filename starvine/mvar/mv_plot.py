8a99dd9da4031003b701e791026293881323bb75
William Gurecky <william.gurecky@gmail.com>
2018-05-17 17:02:15 -0500

    WIP: handle poor default scaling behavior of seaborn

--- starvine/mvar/mv_plot.py
+++ starvine/mvar/mv_plot.py
@@ -6,13 +6,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def matrixPairPlot(data, weights=None, corr_stat="kendalltau", **kwargs):
+def matrixPairPlot(data_in, weights=None, corr_stat="kendalltau", **kwargs):
     """!
     @brief Plots a matrix of pair plots.
     @param data <pandas dataframe> nDim data set
     @param weights np_1darray of weights to assign to each row in data
     @param corr_stat (optional) correlation statistic for plot
     """
+    sns.set(font_scale=kwargs.get("font_scale", 1.0))
+    if kwargs.get("rescale", False):
+        data = data_in * 1e4
+    else:
+        data = data_in
     upper_kde = kwargs.pop("kde", False)
     pair_plot = sns.PairGrid(data, palette=["red"], size=kwargs.pop("size", 5))
     # UPPER
@@ -31,21 +36,25 @@ def matrixPairPlot(data, weights=None, corr_stat="kendalltau", **kwargs):
     # pt_size = 25. * np.log(len(weightArray)) * (weightArray - np.min(weightArray)) / (np.max(weightArray) + 0.01 - np.min(weightArray)) + 0.2
     pt_size = 5.0 / np.log(data.shape[0]) * weightArray / meanWeight
     pt_size = np.clip(pt_size, 0.1, 50.)
+    pt_size = kwargs.get("pt_size", pt_size)
     pair_plot.map_lower(plt.scatter, s=pt_size)
     pair_plot.map_lower(corrfunc, cstat=corr_stat)
     #
     # DIAG
-    # pair_plot.map_diag(sns.distplot, kde=True, norm_hist=True, hist_kws={'weights': weightArray})
-    pair_plot.map_diag(plt.hist, edgecolor="white", weights=weightArray)
-    #
-    # set Y axes
-    # pair_plot.axes[-1, 0].set_ylim((0.096, 0.104))
-    pair_plot.axes[-1, 0].autoscale(tight=True)
-    plt.ticklabel_format(style='sci', scilimits=(0,0))
+    if kwargs.get("diag_hist", True):
+        pair_plot.map_diag(plt.hist, edgecolor="white", weights=weightArray)
+    else:
+        pair_plot.map_diag(sns.distplot, kde=True, norm_hist=True, hist_kws={'weights': weightArray})
+    # sci notation
+    tril_index = np.tril_indices(data.shape[-1])
+    for tri_l_i, tri_l_j in zip(tril_index[0], tril_index[1]):
+        axis = pair_plot.axes[tri_l_i, tri_l_j]
+        axis.ticklabel_format(style='sci', scilimits=(0,0), useMathText=True)
     outfile = kwargs.pop("savefig", None)
     if outfile:
         pair_plot.savefig(outfile)
     # plt.close()
+    sns.set(font_scale=1.0)
     return pair_plot
 
 
@@ -56,8 +65,10 @@ def xy_slope(x, y, **kws):
                 xy=(0.05, 0.95), xycoords=ax.transAxes)
     ax.annotate("y0= {:.3e}".format(intercept),
                 xy=(0.05, 0.895), xycoords=ax.transAxes)
-    ax.annotate("R^2= {:.2f}".format(r_squared),
+    ax.annotate(r"$R^2$= {:.2f}".format(r_squared),
                 xy=(0.75, 0.95), xycoords=ax.transAxes)
+    ax.annotate(r"$p$= {:.2f}".format(p),
+                xy=(0.75, 0.895), xycoords=ax.transAxes)
 
 
 def corrfunc(x, y, **kws):
@@ -69,7 +80,7 @@ def corrfunc(x, y, **kws):
     r, _ = stat_func(x, y)
     ax = plt.gca()
     if cstat is "kendalltau":
-        ax.annotate("kTau= {:.2f}".format(r),
+        ax.annotate(r"$\rho_\tau$= {:.2f}".format(r),
                     xy=(0.05, 0.95), xycoords=ax.transAxes)
     if cstat is "pearsonr":
         ax.annotate("PsRho= {:.2f}".format(r),
