0db86acc222d1af3f85fa1c749f54a6d45ea2091
William Gurecky <william.gurecky@gmail.com>
2020-07-25 05:55:48 -0500

    fix bug in mixuture copula parameter init

--- starvine/bvcopula/tests/test_mixture_copula.py
+++ starvine/bvcopula/tests/test_mixture_copula.py
@@ -6,7 +6,7 @@ from scipy.stats.mstats import rankdata
 # COPULA IMPORTS
 from starvine.bvcopula.copula.mixture_copula import MixtureCopula as mc
 import numpy as np
-import matplotlib.pyplot as plt
+from starvine.bvcopula import bv_plot
 import os
 pwd_ = os.path.dirname(os.path.abspath(__file__))
 from starvine.bvcopula.copula.gumbel_copula import GumbelCopula
@@ -17,14 +17,17 @@ tol = 4e-2
 
 class TestMixtureCopula(unittest.TestCase):
     def setUp(self):
-        self._mix_copula = mc(GumbelCopula(1), 0.5,
-                              GumbelCopula(2), 0.5)
+        self._mix_copula = mc(GumbelCopula(2, [2.1]), 0.5,
+                              GumbelCopula(3, [2.7]), 0.5)
 
     def testMixCoplulaPdf(self):
-        u = np.linspace(1.0e-8, 1.0-1e-8, 50)
-        v = np.linspace(1.0e-8, 1.0-1e-8, 50)
-        c_pdf = self._mix_copula.pdf(u, v)
+        u = np.linspace(6.0e-2, 1.0-6e-2, 50)
+        v = np.linspace(6.0e-2, 1.0-6e-2, 50)
+        uu, vv = np.meshgrid(u, v)
+        c_pdf = self._mix_copula.pdf(uu.flatten(), vv.flatten())
         self.assertTrue(np.all(c_pdf >= 0))
+        # plot mixture pdf
+        bv_plot.bvContourf(uu.flatten(), vv.flatten(), c_pdf, savefig="mix.png")
 
     def testMixCoplulaCdf(self):
         u = np.linspace(1.0e-8, 1.0-1e-8, 50)
